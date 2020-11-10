f266262a65cff563c0879ec078e7ff4d865028f1
duyle <duy.leduc8@gmail.com>
2020-11-10 10:34:53 +0700

    obtain, organize and process the data

--- app.py
+++ app.py
@@ -44,5 +44,39 @@ mobile = tf.keras.applications.mobilenet.MobileNet()
 # predictions = mobile.predict(preprocessed_image)
 
 # * Get results and get top X predictions (default is 5)
-results = imagenet_utils.decode_predictions(predictions)
-print(results)
+# results = imagenet_utils.decode_predictions(predictions)
+# print(results)
+
+# * Distributed images to train, valid and test folder
+os.chdir('data/Sign-Language-Digits-Dataset')
+if os.path.isdir('train/0/') is False:
+    os.mkdir('train')
+    os.mkdir('valid')
+    os.mkdir('test')
+
+    for i in range(0, 10):
+        shutil.move(f'{i}', 'train')
+        os.mkdir(f'valid/{i}')
+        os.mkdir(f'test/{i}')
+
+        valid_samples = random.sample(os.listdir(f'train/{i}'), 30)
+        for j in valid_samples:
+            shutil.move(f'train/{i}/{j}', f'valid/{i}')
+
+        test_samples = random.sample(os.listdir(f'train/{i}'), 10)
+        for k in test_samples:
+            shutil.move(f'train/{i}/{k}', f'test/{i}')
+os.chdir('../../')
+
+# * Process the data
+train_path = 'data/Sign-Language-Digits-Dataset/train'
+valid_path = 'data/Sign-Language-Digits-Dataset/valid'
+test_path = 'data/Sign-Language-Digits-Dataset/test'
+
+train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
+    directory=train_path, target_size=(224, 224), batch_size=10)
+valid_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
+    directory=valid_path, target_size=(224, 224), batch_size=10)
+# ? In test batches, we set shuffle to False, so that we can access the corresponding non-shuffle test labels to plot in the confustion matrix
+test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
+    directory=test_path, target_size=(224, 224), batch_size=10, shuffle=False)
